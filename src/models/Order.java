a2456b2e99906b69a3c5700e216109707677155b
kerem <kerem@192.168.1.25>
2025-12-25 22:52:39 +0300

    order classının bir kısmını yazdım

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,5 +1,21 @@
 package models;
+import payment.PaymentMethod;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
+    private Customer customer;
+    private List<MenuItem> items;
+    private double totalAmount;
 
-}
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.items = new ArrayList<>();
+        this.totalAmount = 0.0;
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+        totalAmount += item.getPrice();
+        System.out.println(item.getName() + " sepete eklendi.");
+    }
