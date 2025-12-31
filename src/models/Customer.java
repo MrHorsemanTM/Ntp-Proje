d51893000dddc6e3839da4d551cce9e4fb7f1474
kerem <kerem@192.168.1.25>
2025-12-31 15:25:43 +0300

    customer ve user classlarını güncelledim

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -2,27 +2,26 @@ package models;
 
 import ınterface.Orderable;
 
-
 public class Customer extends User implements Orderable {
- 
     private String address;
     private String phoneNumber;
 
-    public Customer(String name, String email, String address, String phoneNumber) {
-        super(name, email);
+    public Customer(String name, String surname, String username, String password, String address, String phoneNumber) {
+        super(name, surname, username, password);
         this.address = address;
         this.phoneNumber = phoneNumber;
     }
 
- 
     @Override
     public void placeOrder() {
-        System.out.println(name + " kullanıcısı (" + email + ") siparişini onayladı.");
+        System.out.println("Sayın " + name + " " + surname + ", siparişiniz onaylandı.");
         System.out.println("Teslimat Adresi: " + address);
         System.out.println("İletişim: " + phoneNumber);
     }
     
+    public String toFileFormat() {
+        return username + "," + password + "," + name + "," + surname + "," + phoneNumber + "," + address;
+    }
 
     public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -2,12 +2,19 @@ package models;
 
 public class User {
     protected String name;
-    protected String email;
+    protected String surname;
+    protected String username;
+    protected String password;
 
-    public User(String name, String email) {
+    public User(String name, String surname, String username, String password) {
         this.name = name;
-        this.email = email;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
     }
 
     public String getName() { return name; }
+    public String getSurname() { return surname; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
 }
