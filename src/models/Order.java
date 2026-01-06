fcf207c69d9658a1470d49e2e7e9939db2dcf3ba
kerem <kerem@192.168.1.25>
2026-01-06 20:44:57 +0300

    Order classını kuponlar kullanılailsin diye güncelledim

--- src/models/Order.java
+++ src/models/Order.java
@@ -8,11 +8,13 @@ public class Order {
     private Customer customer;
     private List<MenuItem> items;
     private double totalAmount;
+    private Coupon appliedCoupon; 
 
     public Order(Customer customer) {
         this.customer = customer;
         this.items = new ArrayList<>();
         this.totalAmount = 0.0;
+        this.appliedCoupon = null; 
     }
 
     public void addItem(MenuItem item) {
@@ -20,17 +22,45 @@ public class Order {
         totalAmount += item.getPrice();
         System.out.println(item.getName() + " sepete eklendi.");
     }
+ 
+    public boolean applyCoupon(Coupon coupon) {
+        if (this.appliedCoupon != null) {
+            System.out.println("Zaten bir kupon tanımlı!");
+            return false;
+        }
+        this.appliedCoupon = coupon;
+        return true;
+    }
+
+    public double getDiscountedAmount() {
+        if (appliedCoupon == null) {
+            return totalAmount;
+        }
+        double discount = (totalAmount * appliedCoupon.getDiscountPercentage()) / 100;
+        return totalAmount - discount;
+    }
 
     public void showOrderSummary() {
         System.out.println("\n--- Sipariş Özeti ---");
         if(items.isEmpty()) {
             System.out.println("Sepetiniz boş.");
-        } else {
-            for (MenuItem item : items) {
-                System.out.println(item.getName() + ": " + item.getPrice() + " TL");
-            }
+            return;
+        } 
+        
+        for (MenuItem item : items) {
+            System.out.println(item.getName() + ": " + item.getPrice() + " TL");
         }
-        System.out.println("Toplam Tutar: " + totalAmount + " TL");
+        
+        System.out.println("-------------------------");
+        System.out.println("Ara Toplam: " + totalAmount + " TL");
+
+        if (appliedCoupon != null) {
+            double discountValue = totalAmount - getDiscountedAmount();
+            System.out.println("Kupon İndirimi (" + appliedCoupon.getCode() + "): -" + discountValue + " TL");
+            System.out.println("ÖDENECEK TUTAR: " + getDiscountedAmount() + " TL");
+        } else {
+            System.out.println("ÖDENECEK TUTAR: " + totalAmount + " TL");
+        }
     }
 
     public void completeOrder(PaymentMethod paymentMethod) {
@@ -39,7 +69,7 @@ public class Order {
             return;
         }
         customer.placeOrder();
-        paymentMethod.pay(totalAmount);
+        paymentMethod.pay(getDiscountedAmount());
         System.out.println("Sipariş başarıyla oluşturuldu!\n");
     }
 
